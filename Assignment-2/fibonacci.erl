502486b8c3a733d940412934a63046d6a6b558c9
mishal23 <shahmishal1998@gmail.com>
2017-05-31 02:01:51 +0530

    Done

--- Assignment-2/fibonacci.erl
+++ Assignment-2/fibonacci.erl
@@ -1,6 +1,24 @@
 -module (fibonacci).
--export ([fib/1]).
+-export ([tfib/1,ntfib/1]).
 
-fib(1) -> 0;
-fib(2) -> 1;
-fib(N) when N>0 -> fib(N-1) + fib(N-2);
+% NonTail Recursive
+
+ntfib(1) -> 1;
+ntfib(2) -> 1;
+ntfib(N) -> ntfib(N-1) + ntfib(N-2).
+
+
+% Tail Recursive 
+
+tfib(1) -> io:format("~p~n",[[1]]);
+tfib(2) -> io:format("~p~n",[[1,1]]);
+tfib(N) when N > 2 -> 
+     hd(fibo(N-1,[1,1]) ).
+
+% for numbers greater than 2
+fibo(1,L) ->
+	lists:reverse(L),
+    L;
+fibo(N,L) ->
+    [H1,H2|_T] = L,
+    fibo(N-1,[H1 + H2 | L]).
