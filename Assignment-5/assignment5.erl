d53d7a5fb49f3ce03db2cff32705a513b537b731
mishal23 <shahmishal1998@gmail.com>
2017-07-08 00:29:22 +0530

    Assignment-5 updated

--- Assignment-5/assignment5.erl
+++ Assignment-5/assignment5.erl
@@ -1,27 +1,36 @@
 -module (assignment5).
--export ([add/1,subtract/1,multiply/1,divide/1,greaterThanX/1,lessThanX/1,derive/2,integral/4,sum/1]).
+-export ([add/1,subtract/1,multiply/1,divide/1,greaterThanX/1,lessThanX/1,three_a/2,three_b/1,three_c/1,derive/2,integral/4,sum/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%  QUESTION 1  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Part A %
+
+% Part 1 %
 add(X) -> fun(Y) -> X+Y end.
 subtract(X) -> fun(Y) -> X-Y end.
 multiply(X) -> fun(Y) -> X*Y end.
 divide(X) -> fun(Y) -> X/Y end.
 
-% Part B %
+% Part 2 %
 greaterThanX(X) -> fun(Y) -> case Y>X of true -> true; false -> false end end.
 lessThanX(X) -> fun(Y) -> case Y<X of true -> true; false -> false end end.
+
+% Part 3 %
+three_a(Function,Factor) -> fun(List) -> lists:foldl(Function,Factor,List) end.
+three_b(Function) -> fun(List) -> lists:filter(Function,List) end.
+three_c(Function) -> fun(List) -> lists:map(Function,List) end.
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%  QUESTION 2  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 derive(F,H) -> fun(X) -> (F(X+H) - F(X))/H end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%  QUESTION 3  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 integral(A,B,N,F) -> integral(A,B,N,F,0,0).
 
 integral(A,B,N,_,X,Ans) when X>N -> (((B-A)/N)*Ans)/3;
