2f4f9d41ae0bc0400d55ed5c2bcf48f99c9bc747
mishal23 <shahmishal1998@gmail.com>
2017-08-04 18:21:52 +0530

    Client Greet Client-Server Implemented

--- Client Greet/README.md
+++ Client Greet/README.md
@@ -1,4 +1,59 @@
 # Client Greet
+- The server is sent the request to perform the task from the client based on the input(Concurrent Programming).
 - The program greets the user based on the time and date such as Morning,Afternoon,Evening,Night.
 - Also the program has 3 set prototypes for holidays and one normal day set, so if it's that day, it greets the user with the message
 - The set holidays are Uttarayan, Christmas and New Year.
+
+### Demo
+```
+1> greet:start().
+CLIENT GREET PROGRAM
+To know the date,type : date.
+To know the time,type : time.
+To know both date and time type : both.
+type: date.
+<CLIENT>Server requested for Date
+<SERVER>Server received Date request
+<0.241.0>
+Date(dd/mm/yyyy):- 4/8/2017
+Just a normal day
+
+2> greet:start().
+      
+CLIENT GREET PROGRAM
+To know the date,type : date.
+To know the time,type : time.
+To know both date and time type : both.
+type: time.
+<CLIENT>Server requested for Time
+<SERVER>Server received Time request
+<0.245.0>
+Time(hh:mm:ss):- 18:18:37
+Evening
+
+3> greet:start().
+      
+CLIENT GREET PROGRAM
+To know the date,type : date.
+To know the time,type : time.
+To know both date and time type : both.
+type: both.
+<CLIENT>Server requested for Date & Time
+<SERVER>Server received Date & Time request
+<0.249.0>
+Time(hh:mm:ss):- 18:18:42
+Evening
+Date(dd/mm/yyyy):- 4/8/2017
+Just a normal day
+
+4> greet:start().
+      
+CLIENT GREET PROGRAM
+To know the date,type : date.
+To know the time,type : time.
+To know both date and time type : both.
+type: arbi.
+<CLIENT>Input sent to server
+<SERVER>Improper request received from client
+<0.253.0>
+```
--- Client Greet/greet.erl
+++ Client Greet/greet.erl
@@ -1,16 +1,48 @@
 -module (greet).
--export ([greet_time/0,time1/0,date1/0,greet_date/0,start/0]).
+-export ([greet_time/0,time1/0,date1/0,greet_date/0,start/0,client/2,server/0,greet_all/0]).
 
-% Function to return time
+%% Client Code %%
+client(Input,Server_PID) when Input==date -> Server_PID ! date,
+											io:format("<CLIENT>Server requested for Date~n");
+client(Input,Server_PID) when Input==time -> Server_PID ! time,
+											io:format("<CLIENT>Server requested for Time~n");
+client(Input,Server_PID) when Input==both -> Server_PID ! both,
+											io:format("<CLIENT>Server requested for Date & Time~n");
+
+client(Input,Server_PID) -> Server_PID ! Input,
+							io:format("<CLIENT>Input sent to server~n").
+
+
+%% Server Code %%
+server() ->
+    receive
+        date ->
+        	io:format("<SERVER>Server received Date request~n", []),
+        	spawn(greet,greet_date,[]);
+            
+        time ->
+        	io:format("<SERVER>Server received Time request~n",[]),
+        	spawn(greet,greet_time,[]);
+        	
+        both ->
+        	io:format("<SERVER>Server received Date & Time request~n"),
+        	spawn(greet,greet_all,[]);
+
+        _ -> 
+        	io:format("<SERVER>Improper request received from client~n")
+    end.
+
+
+%% Function to return time in list format %%
 time1() -> tuple_to_list(erlang:time()).
 			
 
-% Function to return date
+%% Function to return date in list format %%
 date1() -> tuple_to_list(erlang:date()).
 	
-
+%% Function to greet user with Time %%
 greet_time() -> [Ht|Tt] = time1(),
-			io:format("Time:- ~p:~p:~p~n",[Ht,hd(Tt),lists:last(Tt)]),
+			io:format("Time(hh:mm:ss):- ~p:~p:~p~n",[Ht,hd(Tt),lists:last(Tt)]),
 			case ((Ht<12) and (Ht>=6)) of
 				true -> io:format("Morning~n");
 				false ->
@@ -24,8 +56,9 @@ greet_time() -> [Ht|Tt] = time1(),
 					end
 			end.
 
+%% Function to greet user with date %%
 greet_date() -> [Hd|Td] = date1(),
-			io:format("Date:- ~p:~p:~p~n",[lists:last(Td),hd(Td),Hd]),
+			io:format("Date(dd/mm/yyyy):- ~p/~p/~p~n",[lists:last(Td),hd(Td),Hd]),
 		   	case ((hd(Td)==12) and (lists:last(Td)==25)) of
 				true -> io:format("Merry Christmas~n");
 				false ->
@@ -38,8 +71,13 @@ greet_date() -> [Hd|Td] = date1(),
 							end
 					end
 			end.
-	  
-%greet() -> greet_time(),greet_date().
+
+%% Function to greet user with Date & Time %%
+greet_all() -> greet_time(),greet_date().	  
+
+
 start() -> 
-		spawn(test,greet_date,[]),
-		spawn(test,greet_time,[]).
+		io:format("~nCLIENT GREET PROGRAM~nTo know the date,type : date.~nTo know the time,type : time.~nTo know both date and time type : both.~n"),
+		{ok,Input} = io:read("type: "),
+		Server_PID = spawn(greet, server, []),
+    	spawn(greet, client, [Input,Server_PID]).
